13ab4f71aeec8cedea01234dd9f9366705e39db2
prcarey11 <prcarey11@gmail.com>
2025-09-02 11:59:13 -0700

    chore: edited typo in About Me section

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -24,7 +24,7 @@ export default function About() {
         <p>
           Outside the tech world, I competed in gymnastics and baseball for many of my early years. 
           I've recently retired to rec softball to the competitive spirit alive!
-          And naturally, as a baseball-plying Bay Area native, the team I'm always rooting for is the SF Giants.
+          And naturally, as a baseball-playing Bay Area native, the team I'm always rooting for is the SF Giants.
         </p>
         <p>If you want someone easy to work with who’s driven to build cool, useful things, let’s connect!</p>
 
